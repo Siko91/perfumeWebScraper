6e16d3b73fe8d5439372cbe19ab52737d609146a
Aleksandar Dinkov <alexander.n.dinkov@gmail.com>
2022-10-30 10:47:23 +0200

    refactor

--- scraping/scrapers/fragrantica.com.notes.js
+++ scraping/scrapers/fragrantica.com.notes.js
@@ -1,13 +1,13 @@
 const Crawler = require("crawler");
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 
 main().catch((e) => console.error(e));
 
 async function main() {
   const c = new Crawler({
     maxConnections: 10,
-    callback: function (error, res, done) {
+    callback: (error, res, done) => {
       if (error) {
         console.log(error);
       } else {
@@ -24,18 +24,21 @@ async function main() {
           if (el.name === "h2") {
             currentCategory = elText;
             continue;
-          } 
+          }
 
           notes.push({
             category: currentCategory,
             name: elText,
             href: $(el).attr("href"),
-            img: $(el).find('img').attr("src")
+            img: $(el).find("img").attr("src"),
           });
         }
 
-        const fileToSave = __dirname + "/../storage/json/" + __filename.replace(/^.*[\\\/]/, '').replace(".js", ".json");
-        fs.writeFileSync(fileToSave, JSON.stringify(notes, null, 2))
+        const fileToSave =
+          __dirname +
+          "/../storage/json/" +
+          __filename.replace(/^.*[\\\/]/, "").replace(".js", ".json");
+        fs.writeFileSync(fileToSave, JSON.stringify(notes, null, 2));
       }
       done();
     },
@@ -43,6 +46,8 @@ async function main() {
 
   // HTML manually copied from "https://www.fragrantica.com/notes"
   c.queue({
-    html: fs.readFileSync(__dirname + "/../storage/html/fragrantica.com.notes.html"),
+    html: fs.readFileSync(
+      __dirname + "/../storage/html/fragrantica.com.notes.html"
+    ),
   });
 }
